dd2c9ba561b4e5c0e9814ebe2088efabf39a3dc4
Michael Guo <mickeyg@umich.edu>
2025-04-01 19:11:15 -0400

    added _redirects and base url

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "/",
   server: {
     port: 8000,
+    historyApiFallback: true,
   },
   optimizeDeps: {
     include: ["three"],
