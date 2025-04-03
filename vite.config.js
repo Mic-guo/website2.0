5969958b671028324045f7ad97f38ed68ee35ec2
Michael Guo <mickeyg@umich.edu>
2025-04-03 11:01:23 -0400

    attempt to fix resolve issue in vercel:

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
   server: {
     port: 8000,
     historyApiFallback: true,
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
   },
   optimizeDeps: {
     include: ["three"],
